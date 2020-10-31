8768105bf16424d53d7555c02ad7de3780a0aac1
SrilakshmiS15 <66517938+SrilakshmiS15@users.noreply.github.com>
2020-10-31 10:59:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,12 +11,10 @@ function setup(){
     world = engine.world;
 
     box1 = new Box(200,200,30,30);
-   box2 = new Box(300,200,50,70);
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
     box1.display();
-    box2.display();
 }
